efe1a6948adbc55713bd72aa3426d44423229458
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-07 12:56:32 +0530

    Refactor App component to use class syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import News from "./components/News";
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import News from './components/News'
 
-const App = () => {
-  return (
-    <>
+export default class App extends Component {
+  render() {
+    return (
       <div className="bg-slate-700 w-screen h-full">
         <Navbar />
         <div>
           <News />
         </div>
       </div>
-    </>
-  );
-};
-
-export default App;
+    )
+  }
+}
