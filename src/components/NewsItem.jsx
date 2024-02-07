1a3ba527c753e7636eaf3e1fa6cebec6f0ac0cbb
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-07 12:56:59 +0530

    Refactor NewsItem component to use class syntax and update props

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,34 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-function NewsItem({ data }) {
-  const { title, urlToImage, description, author, publishedAt } = data;
-
-  return (
-    <div className="flex flex-col overflow-hidden border-2 border-white rounded-md w-full mb-8">
-      <div className="h-48 overflow-hidden">
-        <img
-          src={urlToImage}
-          alt="news"
-          className="object-cover w-full h-full hover:scale-110 hover:duration-200"
-        />
-      </div>
-
-      <h2 className="text-white">{title}</h2>
-
-      <p className="text-white">
-        {description}
-      </p>
-
-      <div className="flex items-center justify-between">
-        <div className="flex flex-col items-center">
-          <span className="text-white">{author}</span>
-          <span className="text-white">{publishedAt}</span>
+export default class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div className="flex flex-col overflow-hidden border-2 border-white rounded-md w-full mb-8">
+        <div className="h-48 overflow-hidden">
+          <img
+            src={imageUrl}
+            alt="news"
+            className="object-cover w-full h-full hover:scale-110 hover:duration-200"
+          />
         </div>
 
-        <button className="text-white">Read More</button>
-      </div>
-    </div>
-  );
-}
+        <h2 className="text-white font-bold">{title} ...</h2>
 
-export default NewsItem;
+        <p className="text-white">{description} ....</p>
+
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col items-center">
+            <span className="text-white">author</span>
+            <span className="text-white">publishedAt</span>
+          </div>
+
+          <a href={newsUrl} target="_blank" className="text-white">Read More</a>
+        </div>
+      </div>
+    );
+  }
+}
