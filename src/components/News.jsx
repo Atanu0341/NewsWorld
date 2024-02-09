bfeb75313ad17b62fab5a2e86938135e9a43272a
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-09 11:29:50 +0530

    Refactor News component to use a separate function for updating news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -23,8 +23,9 @@ export default class News extends Component {
       page: 1,
     };
   }
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5deb2b3fc6bb42b6a4d5d191d458a8dd&page=1&pageSize=${this.props.pageSize}`;
+
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5deb2b3fc6bb42b6a4d5d191d458a8dd&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -36,41 +37,24 @@ export default class News extends Component {
     });
   }
 
+  async componentDidMount() {
+    this.updateNews()
+  }
+
   handlePreviousClick = async () => {
     console.log("prev");
-
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5deb2b3fc6bb42b6a4d5d191d458a8dd&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json();
-
     this.setState({
       page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading: false,
     });
+    this.updateNews();
   };
 
   handleNextClick = async () => {
     console.log("next");
-
-    if (
-      !(
-        this.state.page + 1 >
-        Math.ceil(this.state.totalResults / this.props.pageSize)
-      )
-    ) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5deb2b3fc6bb42b6a4d5d191d458a8dd&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true });
-      let data = await fetch(url);
-      let parsedData = await data.json();
-
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles,
-        loading: false,
-      });
-    }
+    this.setState({
+      page: this.state.page + 1,
+    });
+    this.updateNews();
   };
   render() {
     return (
@@ -94,6 +78,8 @@ export default class News extends Component {
                     }
                     imageUrl={element.urlToImage}
                     newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
                   />
                 </div>
               );
