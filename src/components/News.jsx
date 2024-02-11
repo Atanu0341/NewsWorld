efdb1e7ca83b6a1be37e9e1ef43b68f98c362541
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-11 12:03:15 +0530

    Add progress updates and pagination to News component

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -35,16 +35,20 @@ export default class News extends Component {
   }
 
   async updateNews() {
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5deb2b3fc6bb42b6a4d5d191d458a8dd&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
-    console.log(parsedData);
+    this.props.setProgress(70);
+
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
@@ -52,16 +56,18 @@ export default class News extends Component {
   }
 
   fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5deb2b3fc6bb42b6a4d5d191d458a8dd&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const nextPage = this.state.page + 1;
+
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5deb2b3fc6bb42b6a4d5d191d458a8dd&page=${nextPage}&pageSize=${this.props.pageSize}`;
 
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
+
+    this.setState((prevState) => ({
+      articles: prevState.articles.concat(parsedData.articles),
       totalResults: parsedData.totalResults,
-    });
+      page: nextPage,
+    }));
   };
 
   render() {
