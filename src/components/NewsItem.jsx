3e58b76b20e0e5b6ee4c6e8c719120af6eb69b66
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-11 14:05:23 +0530

    Refactor NewsItem component to use functional component

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,30 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date } = this.props;
-    return (
-      <div className="flex flex-col overflow-hidden border-2 border-white rounded-md w-full mb-8">
-        <div className="h-48 overflow-hidden">
-          <img
-            src={imageUrl}
-            alt="news"
-            className="object-cover w-full h-full hover:scale-110 hover:duration-200"
-          />
-        </div>
-
-        <h2 className="text-white font-bold">{title} ...</h2>
-
-        <p className="text-white">{description} ....</p>
-
-        <span className="text-white">
-          By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}
-        </span>
-
-        <a href={newsUrl} target="_blank" className="text-white">
-          Read More
-        </a>
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date } = props;
+  return (
+    <div className="flex flex-col overflow-hidden border-2 border-white rounded-md w-full mb-8">
+      <div className="h-48 overflow-hidden">
+        <img
+          src={imageUrl}
+          alt="news"
+          className="object-cover w-full h-full hover:scale-110 hover:duration-200"
+        />
       </div>
-    );
-  }
-}
+
+      <h2 className="text-white font-bold">{title} ...</h2>
+
+      <p className="text-white">{description} ....</p>
+
+      <span className="text-white">
+        By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}
+      </span>
+
+      <a href={newsUrl} target="_blank" className="text-white">
+        Read More
+      </a>
+    </div>
+  );
+};
+
+export default NewsItem;
