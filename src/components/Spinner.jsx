d13edeef617f74b1b8ff9d93358e39bffd964beb
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-11 14:04:36 +0530

    Refactor Spinner component

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import loading from '../assets/loader.svg';
+import React from "react";
+import loading from "../assets/loader.svg";
 
-export default class Spinner extends Component {
-  render() {
-    return (
-      <div className='flex justify-center items-center'>
-        <img src={loading} alt="loading..." className='mix-blend-multiply' />
-      </div>
-    );
-  }
-}
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <img src={loading} alt="loading..." className="mix-blend-multiply" />
+    </div>
+  );
+};
+
+export default Spinner;
