986ef7c07bf8632bc270e0149d18e1134e81f8f0
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-11 14:04:57 +0530

    Refactor ModeBtn component to use arrow function syntax

--- src/components/ModeBtn.jsx
+++ src/components/ModeBtn.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function ModeBtn() {
+const ModeBtn = () => {
   return (
     <button className='bg-indigo-600 text-white font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-indigo-400 duration-500'>ModeBtn</button>
   )
