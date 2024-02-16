e0876ec98d37a3735d93366e18fc15e87ca395e5
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-16 12:08:54 +0530

    Refactor Navbar component to include dark theme support

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import ModeBtn from "./ModeBtn";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ darkTheme, setDarkTheme }) => {
   let [open, setOpen] = useState(false);
+  
   return (
     <div className="sticky shadow-md w-full top-0 left-0">
-      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
-        <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800">
+      <div className="md:flex items-center justify-between dark:bg-slate-700 bg-white py-4 md:px-10 px-7">
+        <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] dark:text-white text-gray-800">
           <span className="text-3xl text-indigo-600 mr-1 pt-2">
             <ion-icon name="aperture"></ion-icon>
           </span>
@@ -45,7 +46,7 @@ const Navbar = () => {
           <li className="md:ml-8 text-xl md:my-0 my-7 md:p-4 xl:p-0">
             <Link to="/technology">Technology</Link>
           </li>
-          <ModeBtn />
+          <ModeBtn darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
         </ul>
       </div>
     </div>
